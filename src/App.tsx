d2e512d2e4222c2e3c28af59146f69be9c1fa1f4
Andrés Breceda <a.e.breceda@gmail.com>
2025-12-25 13:24:59 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='bg-amber-950'>
+      weeey
+      <div className='bg-amber-400'>Hola mundo</div>
+    </div>
+  )
+}
+
+export default App
